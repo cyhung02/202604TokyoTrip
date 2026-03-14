5f6c9ba04b122abef04c04aa5093a6fb9773777e
cyhung02 <39024511+cyhung02@users.noreply.github.com>
2026-03-14 22:29:33 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Tokyo Trip PWA
 
-const APP_VERSION = '2.11.7';
+const APP_VERSION = '2.11.8';
 const CACHE_NAME = `tokyo-trip-v${APP_VERSION}`;
 const FONT_CACHE = 'tokyo-trip-fonts-v3';
 
